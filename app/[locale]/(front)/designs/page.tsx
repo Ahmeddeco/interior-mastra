0253263a8b687687504059cff8dfbb8d1194ed78
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-07-20 10:39:36 +0300

    reset design page

--- app/[locale]/(front)/designs/page.tsx
+++ app/[locale]/(front)/designs/page.tsx
@@ -1,14 +1,3 @@
-import OurWorks from "@/components/pages/designs/OurWorks"
-
-type Props = {
-	params: Promise<{ locale: "ar" | "en" }>
-}
-
-export default async function DesignPage({ params }: Props) {
-	const locale = (await params).locale
-	return (
-		<>
-			<OurWorks locale={locale} />
-		</>
-	)
+export default function DesignPage() {
+  return <h1>Welcome to Designpage!</h1>;
 }
