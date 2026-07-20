f2d6dc01a7c336c56b70e9e2a1ff463255244851
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-07-20 13:06:32 +0300

    fix database mastra issue and link it with Postgres

--- bot/index.ts
+++ bot/index.ts
@@ -1,17 +1,15 @@
-
 import { Mastra } from '@mastra/core/mastra'
-import { LibSQLStore } from '@mastra/libsql'
-import { MastraCompositeStore } from '@mastra/core/storage'
 import { interiorDesignerAgent } from "./agents/interior-designer-agent"
+import { PostgresStore } from '@mastra/pg'
 
+
+const storage = new PostgresStore({
+  id: 'pg-storage',
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
+})
 
 export const mastra = new Mastra({
   agents: { interiorDesignerAgent },
-  storage: new MastraCompositeStore({
-    id: 'composite-storage',
-    default: new LibSQLStore({
-      id: "mastra-storage",
-      url: "file:./mastra.db",
-    }),
-  })
+  storage,
 })
